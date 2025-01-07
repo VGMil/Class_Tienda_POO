6a669451fe737cfda239626867d8046f98f28629
VGMil <vgmiltonisaac@gmail.com>
2025-01-07 09:31:29 -0500

    first commit

---
+++ src/main/java/com/vgmil/tienda/Tienda.java
@@ -0,0 +1,12 @@
+
+package com.vgmil.tienda;
+/**
+ *
+ * @author V6M1000
+ */
+public class Tienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
