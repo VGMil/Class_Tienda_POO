dc5ba6e49f5d80fb443f16b5e5acf858bbad828d
VGMil <vgmiltonisaac@gmail.com>
2025-01-07 13:16:53 -0500

    [Feat] Agregar Menu de interaccion con el programa

--- src/main/java/com/vgmil/tienda/Tienda.java
+++ src/main/java/com/vgmil/tienda/Tienda.java
@@ -12,10 +12,34 @@ public class Tienda {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         Controlador controlador = new Controlador(leer);
-        //Omitir Menu para pruebas
-        controlador.iniciarCliente();
-        controlador.insertarProductos(1);
-        controlador.imprimirFactura();
         
+       byte opcion = 0;
+       do{
+           System.out.println("Tienda Polimorfica");
+           System.out.println("1. Ingresar Cliente");
+           System.out.println("2. Insertar Productos");
+           System.out.println("3. Imprimir Factura");
+           opcion = leer.nextByte();
+           switch (opcion) {
+               case 1:
+                   controlador.iniciarCliente();
+                   break;
+                case 2:
+                    System.out.println("Ingrese cantidad de Productos a registrar");
+                    byte cantidad = leer.nextByte();
+                   controlador.insertarProductos(cantidad);
+                   break;
+                case 3:
+                   controlador.imprimirFactura();
+                   break;
+                case 4:
+                    System.out.println("Gracias por usar nuestro sistema");
+                   break;
+               default:
+                   System.out.println("Ingrese numero valido");
+           }
+       }while(opcion <= 3);
+        
+       
     }
 }
